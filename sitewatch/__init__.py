57b9b606b1d8c6143f66c7aa54e7f57eb435e5a9
Alexey Shamrin <shamrin@gmail.com>
2020-12-19 20:35:55 +0200

    save reports

--- sitewatch/__init__.py
+++ sitewatch/__init__.py
@@ -1,60 +1,112 @@
 import os
+from typing import Optional
 from dataclasses import dataclass
+from datetime import timedelta, datetime
 import asyncio
 
 import httpx
 import asyncpg
-import datetime
 
 @dataclass
-class Check:
+class Page:
+    id: int
     url: str
-    period: datetime.timedelta
+    period: timedelta
     regex: str
 
-async def fetch_urls():
+@dataclass
+class Report:
+    page: Page
+    sent: datetime
+    elapsed: timedelta
+    status_code: int
+    found: Optional[bool] = None
+
+async def connect_db():
     db = os.environ.get('DATABASE', '')
     assert db.startswith('postgres://')
-    conn = await asyncpg.connect(db)
+    return await asyncpg.connect(db)
+
+async def fetch_urls():
+    conn = await connect_db()
 
     await conn.execute('''
-        CREATE TABLE IF NOT EXISTS urls(
-            id serial PRIMARY KEY,
-            url text UNIQUE,
-            period interval,
+        CREATE TABLE IF NOT EXISTS page(
+            pageid serial PRIMARY KEY,
+            url text NOT NULL,
+            period interval NOT NULL,
             regex text
         )
     ''')
 
     await conn.executemany('''
-        INSERT INTO urls(url, period, regex) VALUES($1, $2, $3)
+        INSERT INTO page(pageid, url, period, regex) VALUES($1, $2, $3, $4)
         ON CONFLICT DO NOTHING
-    ''', [('https://httpbin.org/get', datetime.timedelta(minutes=5), 'httpx'),
-          ('https://google.com', datetime.timedelta(minutes=1), 'moved')])
+    ''', [(1, 'https://httpbin.org/get', timedelta(minutes=5), 'httpx'),
+          (2, 'https://google.com', timedelta(minutes=1), None),
+          (3, 'https://google.com', timedelta(seconds=10), 'evil')])
 
-    urls = [Check(r['url'], r['period'], r['regex']) for r in await conn.fetch('SELECT * FROM urls')]
+    pages = [
+        Page(row['pageid'], row['url'], row['period'], row['regex'])
+        for row in await conn.fetch('SELECT * FROM page')
+    ]
 
     await conn.close()
 
-    return urls
+    return pages
 
-async def check(url):
-    sleep = url.period.total_seconds()
+async def save_report(r: Report):
+    conn = await connect_db()
+
+    await conn.execute('''
+        CREATE TABLE IF NOT EXISTS report(
+            responseid serial PRIMARY KEY,
+            pageid integer NOT NULL REFERENCES page,
+            elapsed interval NOT NULL,
+            statuscode int NOT NULL,
+            sent timestamp NOT NULL,
+            found boolean
+        )
+    ''')
+
+    await conn.execute('''
+        INSERT INTO report(pageid, elapsed, statuscode, sent, found)
+        VALUES($1, $2, $3, $4, $5)
+    ''', r.page.id, r.elapsed, r.status_code, r.sent, r.found,
+    )
+
+    await conn.close()
+
+
+async def check(page):
+    sleep = page.period.total_seconds()
     async with httpx.AsyncClient() as client:
         while True:
-            r = await client.get(url.url)
-            print(f'{url.url}:', 'OK' if url.regex in r.text else 'ERROR')
-            print(f'{url.url}:', f'waiting {sleep}s...')
+            now = datetime.now()
+            r = await client.get(page.url)
+            prefix = f'pageid:{page.id} url:{page.url} period:{page.period} regex:{page.regex!r}:'
+            if page.regex is not None:
+                print(prefix, 'OK' if page.regex in r.text else 'ERROR')
+            report = Report(
+                page = page,
+                sent = now,
+                elapsed = r.elapsed,
+                status_code = r.status_code,
+                found = None if page.regex is None else page.regex in r.text,
+            )
+            await save_report(report)
+            print(prefix, f'waiting {sleep}s...')
             await asyncio.sleep(sleep)
 
 async def main():
     print('starting up')
 
-    urls = await fetch_urls()
+    pages = await fetch_urls()
 
-    for url in urls:
-        asyncio.create_task(check(url))
+    for page in pages:
+        asyncio.create_task(check(page))
 
+    # TODO: fix this
     await asyncio.sleep(1000)
 
 def start():
