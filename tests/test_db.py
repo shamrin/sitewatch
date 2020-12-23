f9b8b78f52ff2bf8029f99bd2bfc5b90f2ff6ae5
Alexey Shamrin <shamrin@gmail.com>
2020-12-23 18:10:50 +0200

    test db.listen concurrently

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,28 +1,47 @@
 from unittest.mock import AsyncMock
 
 import pytest
+import trio
 
 from sitewatch.db import listen
 
 
-class MockDbConnection:
-    remove_listener = AsyncMock()
-
-    async def add_listener(self, channel, callback):
-        callback('conn', 'pid', channel, 'payload')
-
-
 @pytest.fixture
-def db_connection():
-    return MockDbConnection()
+async def send_connection(nursery):
+    send = trio.Event()
+
+    class MockDbConnection:
+        remove_listener = AsyncMock()
+        _add_listener_called = False
+
+        async def add_listener(self, channel, callback):
+            async def send_on_event(task_status):
+                task_status.started()
+                await send.wait()
+                callback('conn', 'pid', channel, 'payload')
+
+            await nursery.start(send_on_event)
+            self._add_listener_called = True
+
+    conn = MockDbConnection()
+    return send, conn
 
 
-async def test_listen(db_connection):
-    async with listen(db_connection, 'changes') as channel:
-        async for payload in channel:
-            assert payload == 'payload'
-            break
+async def test_listen(send_connection):
+    send, connection = send_connection
+
+    assert not connection._add_listener_called
+
+    async with listen(connection, 'changes') as changes:
+        # confirm there is nothing to receive at the beginning
+        assert connection._add_listener_called
+        with pytest.raises(trio.WouldBlock):
+            changes.receive_nowait()
+
+        # send notification and confirm its receival
+        send.set()
+        assert await changes.receive() == 'payload'
 
     # assert that remove_listener has been called
-    db_connection.remove_listener.assert_awaited_once()
-    assert db_connection.remove_listener.await_args.args[0] == 'changes'
+    connection.remove_listener.assert_awaited_once()
+    assert connection.remove_listener.await_args.args[0] == 'changes'
