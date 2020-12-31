77d7be5cb3a81a612f102762a490944cb421878d
Alexey Shamrin <shamrin@gmail.com>
2020-12-31 02:47:17 +0200

    wip: start postgres via testcontainers library
    testcontainers library is nice, but:
    
    * it's heavy, requires sqlalchemy and psycopg2
    * returns weird '+psycopg2' connection url
    * synchronous, have to use a thread
    * still doesn't support kafka

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,10 +1,13 @@
+import os
 from unittest.mock import AsyncMock
 
 import pytest
 import trio
+from testcontainers.postgres import PostgresContainer
+import trio_asyncio
 
 from sitewatch.db import listen
-
+from sitewatch.db import connect
 
 @pytest.fixture
 async def db_connection(nursery):
@@ -25,6 +28,45 @@ async def db_connection(nursery):
 
     conn = MockDbConnection()
     return started, conn
+
+
+# https://github.com/Scille/parsec-cloud/blob/b80cdb3922da9bf1614900885d8244b294cafd1c/tests/conftest.py#L260-L272
+@pytest.fixture
+async def asyncio_loop():
+    # When a ^C happens, trio send a Cancelled exception to each running
+    # coroutine. We must protect this one to avoid deadlock if it is cancelled
+    # before another coroutine that uses trio-asyncio.
+    with trio.CancelScope(shield=True):
+        async with trio_asyncio.open_loop() as loop:
+            yield loop
+
+
+@pytest.fixture
+async def connection(asyncio_loop, nursery):
+    def run_db(send_to_trio, stopped):
+        print('starting db')
+        with PostgresContainer("postgres:12.5-alpine") as postgres:
+            trio.from_thread.run(send_to_trio.send, postgres.get_connection_url())
+            trio.from_thread.run(stopped.wait)
+            print('stopping db')
+
+    stopped = trio.Event()
+    send_to_trio, receive_from_thread = trio.open_memory_channel(0)
+    nursery.start_soon(trio.to_thread.run_sync, run_db, send_to_trio, stopped)
+    url = await receive_from_thread.receive()
+    os.environ['PG_SERVICE_URI'] = url.replace('+psycopg2','')
+    async with connect() as conn:
+        yield conn
+    stopped.set()
+
+
+async def test_connection(connection):
+    for row in await connection.fetch('select version()'):
+        print(f'1 {row["version"]=}')
+
+async def test_connection2(connection):
+    for row in await connection.fetch('select version()'):
+        print(f'2 {row["version"]=}')
 
 
 async def test_listen(db_connection):
